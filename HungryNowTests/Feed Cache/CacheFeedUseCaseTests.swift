a0adc5eb057fe4c5ba586ca602ef18a4fb2bc0c2
MiraEs <15828125+MiraEs@users.noreply.github.com>
2024-02-08 16:37:40 -0500

    save command requests insertion on successful deletion

--- HungryNowTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ HungryNowTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -9,20 +9,36 @@ class LocalMealFeedLoader {
     }
     
     func save(_ items: [MealFeedItem]) {
-        store.deleteCachedFeed()
+        store.deleteCachedFeed { [unowned self] error in
+            if error == nil {
+                self.store.insert(items)
+            }
+        }
     }
 }
 
 class FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    
     var deleteCachedFeedCallCount = 0
     var insertCallCount = 0
     
-    func deleteCachedFeed() {
+    private var deletionCompletions = [DeletionCompletion]()
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         deleteCachedFeedCallCount += 1
+        deletionCompletions.append(completion)
     }
     
     func completeDeletion(with error: Error, at index: Int = 0) {
-        
+        deletionCompletions[index](error)
+    }
+    
+    func completeDeletionSuccessfully(at index: Int = 0) {
+        deletionCompletions[index](nil)
+    }
+    
+    func insert(_ items: [MealFeedItem]) {
+        insertCallCount += 1
     }
 }
 
@@ -50,6 +66,16 @@ class CacheFeedUseCaseTests: XCTestCase {
         store.completeDeletion(with: deletionError)
         
         XCTAssertEqual(store.insertCallCount, 0)
+    }
+    
+    func test_save_requestsNewCacheInsertionOnSuccessfulDeletion() {
+        let (sut, store) = makeSUT()
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+        store.completeDeletionSuccessfully()
+        XCTAssertEqual(store.insertCallCount, 1)
+        
     }
     
     // MARK: - Helper
